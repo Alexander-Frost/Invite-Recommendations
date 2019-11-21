7a5fdf91d11eb42f9fbe8efd4b354cb999ff2e13
Alex <alexcc17@gmail.com>
2019-11-21 00:19:10 -0500

    Initial Commit

---
+++ FriendSuggestions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendSuggestions
+//
+//  Created by Alex on 11/21/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
