21b84a0fabda1432b2afab5812e8adaf3685d7af
Alex <alexcc17@gmail.com>
2019-11-22 14:11:16 -0500

    Display contacts

--- FriendSuggestions/View Controllers/ViewController.swift
+++ FriendSuggestions/View Controllers/ViewController.swift
@@ -23,12 +23,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        tableView.dataSource = self
+        tableView.delegate = self
         
         print(contactsController.contacts.count, contactsController.imageContacts.count)
     }
+}
 
-
+extension ViewController: UITableViewDelegate {
     
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contactsController.imageContacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("SuggestionsTableViewCell", owner: self, options: nil)?.first as! SuggestionsTableViewCell
+        cell.friendSuggestion = contactsController.imageContacts[indexPath.row]
+        return cell
+    }
+    
+}
--- FriendSuggestions/Views/SuggestionsTableViewCell.swift
+++ FriendSuggestions/Views/SuggestionsTableViewCell.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class SuggestionsTableViewCell: UITableViewCell {
 
+    // MARK: - Properties
+    
+    var friendSuggestion: Contacts? {
+        didSet {
+            if friendSuggestion != nil {
+                updateViews(contact: friendSuggestion!)
+            }
+        }
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var subtitleLbl: UILabel!
+    @IBOutlet weak var initialsLbl: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    // MARK: - Actions
+    
+    @IBAction func addBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - VC Lifecycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +47,30 @@ class SuggestionsTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // MARK: - Methods
+    
+    private func updateViews(contact: Contacts){
+        setupUI(contact: contact)
+        nameLbl.text = contact.givenName + " " + contact.familyName
+        userImageView.image = contact.avatar
+    }
+ 
+    private func setupUI(contact: Contacts){
+        
+        // Intials Label
+        if contact.avatar == nil {
+            initialsLbl.isHidden = false
+            let name = contact.givenName + " " + contact.familyName
+            initialsLbl.text = name.initials
+        } else {
+            initialsLbl.isHidden = true
+        }
+    }
+    
+}
+
+extension String {
+    var initials: String {
+        return self.components(separatedBy: " ").filter { !$0.isEmpty }.reduce("") { ($0 == "" ? "" : "\($0.first!)") + "\($1.first!)" }.uppercased()
+    }
 }
